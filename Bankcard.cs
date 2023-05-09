416cef1c3cd5fd0cfc495810bbd9137b9d46fc54
Petra Korhonen <peptekki@outlook.com>
2023-05-09 11:52:27 +0300

    Add project files.

---
+++ Bankcard.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maksukorttitehtava
+{
+    class Bankcard
+    {
+        private double saldo;
+        private double moneyLoad;
+        private double eatCheapAmmount = 2.60;
+        private double eatGoodAmmount = 4.60;
+        public Bankcard(double saldo) 
+        {
+            this.saldo = saldo;
+        }
+        public override string ToString()
+        {
+            return $"Balance on the card {saldo.ToString("F")} €.";
+        }
+        public void eatChep()
+        {
+            if (saldo - eatCheapAmmount > 0)
+                this.saldo = saldo - eatCheapAmmount;
+        }
+        public void eatGood() 
+        {
+            if (saldo - eatGoodAmmount >0)
+                this.saldo = saldo - 4.60;
+        }
+        public void LoadMoney(double moneyLoad)
+        { 
+            this.moneyLoad = moneyLoad;
+            if (moneyLoad > 0)
+                this.saldo = saldo + moneyLoad;
+            else 
+                this.saldo = saldo;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using Maksukorttitehtava;
+
+
+Bankcard pekkaCard = new Bankcard(20);
+Bankcard mattiCard = new Bankcard(30);
+Console.WriteLine("Pekka: " + pekkaCard);
+Console.WriteLine("Matti: " + mattiCard);
+pekkaCard.eatGood();
+mattiCard.eatChep();
+Console.WriteLine("Pekka: " + pekkaCard);
+Console.WriteLine("Matti: " + mattiCard);
+pekkaCard.LoadMoney(20);
+mattiCard.eatGood();
+Console.WriteLine("Pekka: " + pekkaCard);
+Console.WriteLine("Matti: " + mattiCard);
+pekkaCard.eatChep();
+pekkaCard.eatChep();
+mattiCard.LoadMoney(50);
+Console.WriteLine("Pekka: " + pekkaCard);
+Console.WriteLine("Matti: " + mattiCard);
